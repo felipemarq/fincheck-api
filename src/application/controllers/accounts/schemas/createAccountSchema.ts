6b3b933e6136f58c8f83a3f83ffa3d7f9d547f4b
Felipe Marques <felipeshay2000@gmail.com>
2026-01-23 16:57:14 -0300

    fix: :bug: Retirado a obrigação de um saldo inicial na conta maior que 0

--- src/application/controllers/accounts/schemas/createAccountSchema.ts
+++ src/application/controllers/accounts/schemas/createAccountSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const createAccountSchema = z.object({
   entityId: z.string().min(1, "Id da entidade é obrigatório"),
-  initialBalance: z.number().min(1, "Saldo inicial é obrigatório"),
+  initialBalance: z.number(),
   name: z.string().min(1, "Nome é obrigatório"),
   type: z.nativeEnum(Account.Type),
   color: z.string().optional(),
